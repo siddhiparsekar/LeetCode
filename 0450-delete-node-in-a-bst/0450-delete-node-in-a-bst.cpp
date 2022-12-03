709d49c919eb2ac3d68a3f2289dc0d11a1698001
Siddhi Parsekar <111981040+siddhiparsekar@users.noreply.github.com>
2022-12-03 23:33:49 +0530

    Time: 71 ms (46.69%), Space: 32.7 MB (42.60%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -13,49 +13,66 @@ class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
         
-        if(root==NULL)return NULL;
+//         if(root==NULL)return NULL;
         
-        if(root->val==key)
-            return helper(root);
+//         if(root->val==key)
+//             return helper(root);
         
-        TreeNode* dummy =root;
-        while (root!=NULL){
+//         TreeNode* dummy =root;
+//         while (root!=NULL){
             
-          if(root->val>key){
-            if(root->left!=NULL && root->left->val==key){
-                root->left=helper(root->left);
-                break;
-            }
-            else{
-                root=root->left;
-            }
-          }
-          else{
-            if(root->right!=NULL && root->right->val==key){
-                root->right=helper(root->right);
-                break;
-            }
-            else{
-                root=root->right;
-            }
-          }
-        }
-        return dummy;
-    }
-    TreeNode* helper(TreeNode* root){
-        if(root->left == NULL)
-            return root->right;
-        else if(root->right == NULL)
-            return root->left;
+//           if(root->val>key){
+//             if(root->left!=NULL && root->left->val==key){
+//                 root->left=helper(root->left);
+//                 break;
+//             }
+//             else{
+//                 root=root->left;
+//             }
+//           }
+//           else{
+//             if(root->right!=NULL && root->right->val==key){
+//                 root->right=helper(root->right);
+//                 break;
+//             }
+//             else{
+//                 root=root->right;
+//             }
+//           }
+//         }
+//         return dummy;
+//     }
+//     TreeNode* helper(TreeNode* root){
+//         if(root->left == NULL)
+//             return root->right;
+//         else if(root->right == NULL)
+//             return root->left;
         
-        TreeNode* rightchild = root->right;
-        TreeNode* lastright = findlastright(root->left);
-        lastright->right = rightchild;
-        return root->left; 
-    }
-    TreeNode* findlastright(TreeNode* root){
-        if(root->right == NULL)
-            return root;
-        return findlastright(root->right);
+//         TreeNode* rightchild = root->right;
+//         TreeNode* lastright = findlastright(root->left);
+//         lastright->right = rightchild;
+//         return root->left; 
+//     }
+//     TreeNode* findlastright(TreeNode* root){
+//         if(root->right == NULL)
+//             return root;
+//         return findlastright(root->right);
+//     }
+        
+        if(root){ 
+            if(key < root->val) root->left = deleteNode(root->left, key);     //We frecursively call the function until we find the target node
+            else if(key > root->val) root->right = deleteNode(root->right, key);       
+            else{
+                if(!root->left && !root->right) return NULL;          //No child condition
+                if (!root->left || !root->right)
+                    return root->left ? root->left : root->right;    //One child contion -> replace the node with it's child
+					                                                //Two child condition   
+                TreeNode* temp = root->left;                        //(or) TreeNode *temp = root->right;
+                while(temp->right != NULL) temp = temp->right;     //      while(temp->left != NULL) temp = temp->left;
+                root->val = temp->val;                            //       root->val = temp->val;
+                root->left = deleteNode(root->left, temp->val);  //        root->right = deleteNode(root->right, temp);		
+            }
+        }
+        return root;
     }
 };
