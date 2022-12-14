bacd32de9f8733792e9733adc5b573f92e438b15
Siddhi <111981040+siddhiparsekar@users.noreply.github.com>
2022-12-14 11:15:34 +0530

    Time: 3 ms (56.21%), Space: 7.8 MB (25.79%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int helper(int idx, vector<int>& nums, vector<int>&dp){
         if(idx<0)return 0;
-        if(idx == 0)return nums[idx];
+        if(idx == 0 )return nums[idx];
         
         if(dp[idx] != -1) return dp[idx];
         int pick = nums[idx] + helper(idx-2, nums, dp);
