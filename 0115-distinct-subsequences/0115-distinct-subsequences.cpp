a59241f3a1ca2da6dba9aed6d94083070dbd064b
Siddhi <111981040+siddhiparsekar@users.noreply.github.com>
2022-12-21 20:00:02 +0530

    Time: 24 ms (94.90%), Space: 6.5 MB (87.75%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,26 +1,46 @@
 class Solution {
 public:
     int numDistinct(string s, string t) {
+        //TABULATION
+//         int n = s.size();
+//         int m = t.size();
+        
+//         vector<double>prev(m+1, 0), curr(m+1, 0);
+        
+//         prev[0] = curr[0] = 1; //first element must be 1 according to base case
+        
+//         for(int i=1; i<=n; i++){
+//             for(int j=1; j<=m; j++){
+                
+//                 if(s[i-1] == t[j-1]){
+//                     curr[j] = prev[j-1] + prev[j];
+//                 }
+//                 else{
+//                     curr[j] = prev[j];
+//                 }
+//             }
+//             prev = curr;
+//         }
+//         return (int)prev[m];
+       
+        
+        //SPACE OPTIMISATION 1D-DP
         int n = s.size();
         int m = t.size();
         
-        vector<double>prev(m+1, 0), curr(m+1, 0);
+        vector<double>dp(m+1, 0);
         
-        prev[0] = curr[0] = 1;
+        dp[0] = 1; //first element must be 1 according to base case
         
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
+            for(int j=m; j>=1; j--){
                 
                 if(s[i-1] == t[j-1]){
-                    curr[j] = prev[j-1] + prev[j];
-                }
-                else{
-                    curr[j] = prev[j];
+                    dp[j] = dp[j-1] + dp[j];
                 }
             }
-            prev = curr;
         }
-        return (int)prev[m];
+        return (int)dp[m];
         
     }
 };
