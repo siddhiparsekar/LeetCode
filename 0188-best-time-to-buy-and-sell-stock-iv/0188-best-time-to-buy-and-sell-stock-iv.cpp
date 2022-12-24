50b0da4bff9aeab48b909aa2e3441cb7a0ea8804
Siddhi <111981040+siddhiparsekar@users.noreply.github.com>
2022-12-24 12:16:12 +0530

    Time: 38 ms (37.09%), Space: 14.5 MB (19.75%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    //1] Approach 1:- MEMOISATION ->3D DP
+    int helper(int idx, int buy, int cap, vector<int>& prices, vector<vector<vector<int>>>&dp) {
+        
+        if(cap == 0)return 0;
+        if(idx == prices.size()) return 0;
+        
+        if(dp[idx][buy][cap] != -1) return dp[idx][buy][cap];
+        int profit = 0;
+        if(buy){
+            profit = max(-prices[idx] + helper(idx+1, 0, cap, prices, dp),
+                        0 + helper(idx+1, 1, cap, prices, dp));
+        }
+        else{
+            profit = max(prices[idx] + helper(idx+1, 1, cap-1, prices, dp),
+                        0 + helper(idx+1, 0, cap, prices, dp));
+        }
+        return dp[idx][buy][cap] = profit;
+    }
+    
+    int maxProfit(int k, vector<int>& prices) {
+        
+        int n = prices.size();
+        vector<vector<vector<int>>>dp(n, vector<vector<int>>(2, vector<int>(k+1, -1)));
+        return helper(0, 1, k, prices, dp);
+
+    }
+};
