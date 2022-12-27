ae88c890a5cf86e541c3ab32e4ca7f54b4ac81e4
Siddhi <111981040+siddhiparsekar@users.noreply.github.com>
2022-12-27 08:02:41 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (15.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int climb(int idx, vector<int>&dp ){
         
-        if(idx<0) return 0;
+        // if(idx<0) return 0;
         if(idx == 1 || idx == 0) return 1;
         
         if(dp[idx] != -1) return dp[idx];
