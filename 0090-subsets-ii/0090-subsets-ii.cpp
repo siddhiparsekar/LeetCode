45817f6061589d286b622bd0e3ee9096484254fc
Siddhi <111981040+siddhiparsekar@users.noreply.github.com>
2022-12-27 18:07:12 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (68.89%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
+// using SET, which takes extra O(logn) to convert it back to a vector.
+    // set<vector<int>> ans;
+    // void findsubsets(int indx, vector<int>& nums, vector<int>ds){
+    //     if(indx==nums.size()){
+    //         ans.insert(ds);
+    //         return;
+    //     }
+    //     findsubsets(indx+1,nums,ds);
+    //     ds.push_back(nums[indx]);
+    //     findsubsets(indx+1,nums,ds);
+    // }
+    // vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+    //     vector<int>ds;
+    //     sort(nums.begin(), nums.end());
+    //     findsubsets(0,nums,ds);
+    //     vector<vector<int>> ans1(ans.begin(), ans.end());
+    //     return ans1;
+    // }
     
+//OPTIMISED ,use while loop to ignore all the following same elements
    void findcomb(int indx, vector<int>&arr, vector<vector<int>> &ans, vector<int>&ds){
         if(indx==arr.size()){
            ans.push_back(ds);
