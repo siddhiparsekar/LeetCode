0dbe35d4fcf30863e205981b0460430b90013cdf
Siddhi <111981040+siddhiparsekar@users.noreply.github.com>
2023-01-07 17:26:33 +0530

    Time: 10 ms (33.76%), Space: 9.6 MB (6.29%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        //Brute force ->linearly traverse O(m*n)
+
+//PS. In LC problem elements are sorted row wise as well as column wise 
+//In GFG problem variant elements are sorted row wise only and not column wise
+        
+    //Brute force ->linearly traverse O(m*n)
         int m = matrix.size(), n= matrix[0].size();
         // for(int i=0; i<m; i++){
         //     for(int j=0; j<n; j++){
@@ -11,16 +15,31 @@ public:
         // }
         // return false;
         
-        //Better Approach(works for gfg variation of this problem as well)
-        int i = 0, j = n-1;
-        while(i<m && j>=0){
-            if(matrix[i][j] == target){
+    //Better Approach(optimal for gfg variant of this problem) -> linear traversal, only one traversal needed O(n)
+        // int i = 0, j = n-1;
+        // while(i<m && j>=0){
+        //     if(matrix[i][j] == target){
+        //         return true;
+        //     }
+        //     else if(matrix[i][j] > target)
+        //         j--;
+        //     else
+        //         i++;
+        // }
+        // return false;
+        
+    //Optimal Approach -> Binary search O(log(m*n)) (not for gfg variant)
+        int low = 0;
+        int high = (m*n) - 1;
+        
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            if(matrix[mid/n][mid%n] == target)
                 return true;
-            }
-            else if(matrix[i][j] > target)
-                j--;
+            else if(matrix[mid/n][mid%n] < target)
+                low = mid + 1;
             else
-                i++;
+                high = mid - 1;
         }
         return false;
     }
